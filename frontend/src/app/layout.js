77177df2708bcccf4c69d995ac4be17a52e2c941
cyril_babu <cyril123654789cyril@gmail.com>
2025-07-21 00:08:34 +0530

    done

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,6 +3,8 @@
 import ContactMe from "./(components)/contact-me";
 import Navbar from "./(components)/Navbar";
 import "./globals.css";
+
+import Head from "next/head";
 // import { useEffect } from "react";
 
 // const generateRandomUserId = () =>
@@ -17,6 +19,22 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en" className="min-h-screen w-full">
+      <head>
+        <title>Cyril Babu&apos;s Blog</title>
+        <link rel="icon" href="/cyril.jpg" />
+      </head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Cyril Babu's personal blog site" />
+      <meta
+        name="keywords"
+        content="Cyril Babu, Blogs, Personal Blog, Web Development"
+      />
+      <meta name="author" content="Cyril Babu" />
+      <meta name="theme-color" content="#131628" />
+      <link rel="apple-touch-icon" href="/cyril.jpg" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+
       <body className="min-h-screen w-full">
         <div className="bg-gray-100 min-h-screen w-full flex flex-col">
           <Navbar />
